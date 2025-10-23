8842aa1b361ca1b3687f8339080e5d043cf7e414
Masahiko Yamada <masa@Mac.lan>
2025-10-23 22:05:18 +0900

    v1.1.2 verbose version

--- src/vumps.jl
+++ src/vumps.jl
@@ -36,22 +36,6 @@ function svd_back(A; η = 1e-40)
     end
 end
 
-ϕ(x) = iszero(x) ? one(x) : sign(x)
-
-function svdfix(A; fix = :U)
-    U, S, V = _svd(A)
-    if fix == :U
-        phase = map(x -> ϕ(x)', vec(sum(U; dims = 1)))
-        U = U * Diagonal(phase)
-        V = V * Diagonal(phase)
-    elseif fix == :V
-        phase = map(x -> ϕ(x)', vec(sum(V; dims = 1)))
-        U = U * Diagonal(phase)
-        V = V * Diagonal(phase)
-    end
-    U, S, V
-end
-
 function leftorth(A, C = Matrix{eltype(A)}(I, size(A, 1), size(A, 1)); tol = 1e-14, kwargs...)
     χ, d, = size(A)
     Q, R = polar(reshape(C * reshape(A, χ, d * χ), χ * d, χ))
@@ -106,69 +90,38 @@ function rightorth(A, C = Matrix{eltype(A)}(I, size(A, 1), size(A, 1)); tol = 1e
     end
 end
 
-function ACproj(AC)
-    χ, d, = size(AC)
-    U, = svdfix(reshape(AC, χ, d * χ); fix = :U)
-    _, _, V = svdfix(reshape(AC, χ * d, χ); fix = :V)
-    ein"(ij, jkl), lm -> ikm"(U', AC, V)
-end
-
-function Sinkhorn(A)
-    n = size(A, 1)
-    F = [exp(2π * im / n * (i - 1) * (j - 1)) / sqrt(n) for i in 1 : n, j in 1 : n]
-    U1 = F' * A * F
-    U2 = [i == 1 && j == 1 ? one(eltype(A)) : (i == 1 || j == 1 ? zero(eltype(A)) : U1[i, j]) for i in 1 : n, j in 1 : n]
-    u, = polar(U2)
-    Anew = F * u * F'
-    Anew * A'
-end
-
 struct UniformMPS <: Manifold end
 
 function Optim.retract!(::UniformMPS, AC; tol = 1e-12)
     χ, d, = size(AC)
-    U0, = svdfix(reshape(AC, χ, d * χ); fix = :U)
-    U, S, V0 = svdfix(reshape(AC, χ * d, χ); fix = :V)
-    AL = ein"ijk, kl -> ijl"(reshape(U, χ, d, χ), U0')
-    C0 = U0 * Diagonal(S) * V0'
-    C, = rightorth(AL, C0; tol = tol)
+    L, C = polar(reshape(AC, χ * d, χ))
+    AL = reshape(L, χ, d, χ)
+    C, = rightorth(AL, C; tol = tol)
     AC .= ein"ijk, kl -> ijl"(AL, C)
-    U, _, V = svdfix(C; fix = :U)
-    L1 = Sinkhorn(U)
-    L2 = Sinkhorn(V)
-    AC .= ein"(ij, jkl), lm -> ikm"(L1, AC, L2')
     AC ./= norm(AC)
 end
 
-function Optim.project_tangent!(::UniformMPS, dAC, AC; η = 1e-40)
-    # O(χ⁴) algorithm by M. G. Yamada
+function Optim.project_tangent!(::UniformMPS, dAC, AC; tol = 1e-12)
     χ, d, = size(AC)
-    U1, S1, V1 = svdfix(reshape(AC, χ, d * χ); fix = :U)
-    U2, S2, V2 = svdfix(reshape(AC, χ * d, χ); fix = :V)
-    S = (S1 .+ S2) ./ 2
-    S² = S .^ 2
-    F = S²' .- S²
-    @. F /= (F ^ 2 + η)
-    dU1 = U1 * (F .* (x -> x .+ x')(U1' * reshape(dAC, χ, d * χ) * V1 * Diagonal(S)))
-    dU1 .-= U1 * Diagonal(im .* imag.(vec(sum(dU1; dims = 1))))
-    dV2 = V2 * (F .* (x -> x .+ x')(Diagonal(S) * U2' * reshape(dAC, χ * d, χ) * V2))
-    dV2 .-= V2 * Diagonal(im .* imag.(vec(sum(dV2; dims = 1))))
-    x = vcat(real.(diag(U1' * reshape(dAC, χ, d * χ) * V1 .- U2' * reshape(dAC, χ * d, χ) * V2)), real.(vec(sum(dU1; dims = 1))), real.(vec(sum(dV2; dims = 1))))
-    A = zeros(3χ, 3χ)
-    function a(x)
-        dac = reshape(U1 * ((x -> x .+ x')(F .* (U1' * transpose(reshape(repeat(x[χ + 1 : 2χ], χ), χ, χ)))) * Diagonal(S) .+ Diagonal(x[1 : χ])) * V1', χ, d, χ) .+ reshape(U2 * (Diagonal(S) * (x -> x .+ x')(F .* (V2' * transpose(reshape(repeat(x[2χ + 1 : end], χ), χ, χ)))) .- Diagonal(x[1 : χ])) * V2', χ, d, χ)
-        du1 = U1 * (F .* (x -> x .+ x')(U1' * reshape(dac, χ, d * χ) * V1 * Diagonal(S)))
-        du1 .-= U1 * Diagonal(im .* imag.(vec(sum(du1; dims = 1))))
-        dv2 = V2 * (F .* (x -> x .+ x')(Diagonal(S) * U2' * reshape(dac, χ * d, χ) * V2))
-        dv2 .-= V2 * Diagonal(im .* imag.(vec(sum(dv2; dims = 1))))
-        vcat(real.(diag(U1' * reshape(dac, χ, d * χ) * V1 .- U2' * reshape(dac, χ * d, χ) * V2)), real.(vec(sum(du1; dims = 1))), real.(vec(sum(dv2; dims = 1))))
+    U1, S1, V1 = svd(reshape(AC, χ, d * χ))
+    U2, S2, V2 = svd(reshape(AC, χ * d, χ) * U1)
+    U2 .= U2 * V2'
+    V2 .= U1
+    sqrtS1 = sqrt.(S1)
+    invsqrtS1 = inv.(sqrtS1)
+    sqrtS2 = sqrt.(S2)
+    invsqrtS2 = inv.(sqrtS2)
+    K1 = Diagonal(invsqrtS1) * (U1' * reshape(dAC, χ, d * χ) * V1) * Diagonal(sqrtS1)
+    K2 = Diagonal(invsqrtS2) * (V2' * reshape(dAC, χ * d, χ)' * U2) * Diagonal(sqrtS2)
+    temp, = linsolve((x -> cat(real(x), imag(x); dims = 3))(K1 .+ K1' .- (K2 .+ K2')); ishermitian = true, isposdef = true, tol = tol, maxiter = 10_000) do x
+        h = x[:, :, 1] .+ im .* x[:, :, 2]
+        dac = reshape(U1 * (Diagonal(invsqrtS1) * (h .+ h') * Diagonal(sqrtS1)) * V1', χ, d, χ) .- reshape(U2 * (Diagonal(sqrtS2) * (h .+ h') * Diagonal(invsqrtS2)) * V2', χ, d, χ)
+        K1 = Diagonal(invsqrtS1) * (U1' * reshape(dac, χ, d * χ) * V1) * Diagonal(sqrtS1)
+        K2 = Diagonal(invsqrtS2) * (V2' * reshape(dac, χ * d, χ)' * U2) * Diagonal(sqrtS2)
+        (x -> cat(real(x), imag(x); dims = 3))(K1 .+ K1' .- (K2 .+ K2'))
     end
-    for i in 1 : 3χ
-        A[:, i] .= a(Matrix{Float64}(I, 3χ, 3χ)[:, i])
-    end
-    U, s, V = svd(A)
-    temp = V[:, 1 : end - 3] * (Diagonal(inv.(s[1 : end - 3])) * (U[:, 1 : end - 3]' * x))
-    dAC .-= reshape(U1 * ((x -> x .+ x')(F .* (U1' * transpose(reshape(repeat(temp[χ + 1 : 2χ], χ), χ, χ)))) * Diagonal(S) .+ Diagonal(temp[1 : χ])) * V1', χ, d, χ) .+ reshape(U2 * (Diagonal(S) * (x -> x .+ x')(F .* (V2' * transpose(reshape(repeat(temp[2χ + 1 : end], χ), χ, χ)))) .- Diagonal(temp[1 : χ])) * V2', χ, d, χ)
+    h = temp[:, :, 1] .+ im .* temp[:, :, 2]
+    dAC .-= reshape(U1 * (Diagonal(invsqrtS1) * (h .+ h') * Diagonal(sqrtS1)) * V1', χ, d, χ) .- reshape(U2 * (Diagonal(sqrtS2) * (h .+ h') * Diagonal(invsqrtS2)) * V2', χ, d, χ)
     dAC .-= AC .* real(dot(AC, dAC))
 end
 
@@ -228,14 +181,14 @@ end
 
 function svumps(h::T, A; tol = 1e-8, iterations = 1000, Hamiltonian = false) where T
     χ, d, = size(A.AL)
-    U, _, V = svdfix(A.C; fix = :U)
+    U, _, V = svd(A.C)
     AC = ein"ij, (jkl, lm) -> ikm"(U', A.AC, V)
 
     function fg!(F, G, x)
-        val, (dx,) = withgradient(x) do y
-            ac = ACproj(y)
-            L, = polar(reshape(ac, χ * d, χ))
-            real(local_energy(reshape(L, χ, d, χ), ac, h))
+        val, (dx,) = withgradient(x) do ac
+            l, c = polar(reshape(ac, χ * d, χ))
+            al = reshape(l, χ, d, χ)
+            real(local_energy(al, ac, h))
         end
         if G !== nothing
             G .= dx
@@ -246,8 +199,7 @@ function svumps(h::T, A; tol = 1e-8, iterations = 1000, Hamiltonian = false) whe
     end
     res = optimize(Optim.only_fg!(fg!), AC, LBFGS(manifold = UniformMPS()), Optim.Options(g_abstol = tol, allow_f_increases = true, iterations = iterations))
 
-    x = Optim.minimizer(res)
-    AC = ACproj(x)
+    AC .= Optim.minimizer(res)
     L, C = polar(reshape(AC, χ * d, χ))
     AL = reshape(L, χ, d, χ)
     _, R = polar(reshape(AC, χ, d * χ); rev = true)
