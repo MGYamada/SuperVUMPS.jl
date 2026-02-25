5b15c019d7b5238b02d64a26b00ac744bb575acd
MGYamada <myspinor@gmail.com>
2026-02-25 17:35:27 +0900

    v1.6.0 go back to the original

--- src/SuperVUMPS.jl
+++ src/SuperVUMPS.jl
@@ -3,12 +3,13 @@ module SuperVUMPS
 using LinearAlgebra
 using OMEinsum
 using Zygote
+using NLSolversBase
+using Optim
 using KrylovKit
 using Printf
 
 export svumps, local_energy, Hamiltonian_construction, canonicalMPS, MixedCanonicalMPS, conjugateMPS
 
 include("vumps.jl")
-include("lbfgs.jl")
 
 end
